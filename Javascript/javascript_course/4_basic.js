5c6287e59471ccaa37f9880879f5fce0acec2820
havel444 <crepamer1@outlook.com>
2022-06-17 12:19:15 -0300

    Javascript phase 05

---
+++ Javascript/javascript_course/4_basic.js
@@ -0,0 +1,53 @@
+// let text = "Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.";
+
+// const buscarPalabra = (palabra, texto) => {
+//   const split = texto.split(" ");
+//   const buscarText = split.includes(palabra);
+//   const resultado =
+//     buscarText == true
+//       ? "la palabra buscado existe"
+//       : "la palabra buscando no existe";
+//   return resultado;
+// };
+
+// console.log(buscarPalabra("tiemp", text));
+
+
+// Relog 
+
+// const text = "hola"
+// console.log(text.length)
+
+// const date = new Date(),
+//   minutes = date.getMinutes(),
+//   hours = date.getHours()
+//   stringHours = new String(hours) // "1"
+//   if(stringHours.legth = 1){
+//     stringHours = `0${stringHours}`
+//   }
+// const relog = `${stringHours}:${minutes}`
+
+// console.log(relog)
+
+
+//todo STRINGS AND NUMBERS
+
+
+//? IMPUT
+// let nombre = prompt('Ingrese un nombre: ');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
