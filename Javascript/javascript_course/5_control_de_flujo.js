99424d464ec8939cabcf591daf3e6d7a2ebf507f
havel444 <crepamer1@outlook.com>
2022-06-20 14:47:20 -0300

    Javascript phase 08

--- Javascript/javascript_course/5_control_de_flujo.js
+++ Javascript/javascript_course/5_control_de_flujo.js
@@ -1,19 +1,50 @@
 //TODO CONTROL DE FLUJO 
 
-//? IF ESLE IF 
+//? IF ESLE ELSE IF
 
 //* IF: BASICO
+//# ejemplo de contraseña para ingresar una pagina
+// let number = Number(prompt('Enter the password: '));
 
-let number = 4;
+// if(number == 222){
+//     alert('Welcome');
+// }else{
+//     alert('Incorrect password');
+// }
 
-if(number == 4){
-    alert('the number is corret');
-}else{
-    alert('The number is incorrect');
+// console.log('asd')
+
+//* IF ELSE IF Y ELSE
+//# Ejemplo basico con la edad sin usar "and" dentro del parentesis
+// let age = Number(prompt('Enter your age: '))
+
+// if(age < 16){
+//         alert('You can not work');
+// }
+// else if(age < 18){
+//     alert('You can work with restrictions');
+// }
+// else if(age < 65){
+//     alert('You can work')
+// }
+// else{
+//     alert('You can retire')
+// }
+
+//? BOOLEAN
+
+//* BOOLEAN: BASICO
+//# usar el comando boolean cuando ya este asignado.
+//# ejemplo: validar si la casa es habitable por su cantidad de baños.
+let aseo = Number(prompt('Ingrese la cantidad: '));
+let casa = (aseo >= 4);
+
+if(casa == true){
+    console.log('La casa es habitable'); 
 }
-
-console.log('asd')
-
+else{
+    console.log('la casa no es habitable');
+}
 
 
 
